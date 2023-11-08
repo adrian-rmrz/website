dfea8d4e6493c1c49fdd337ac721a99ca1a26f6c
Adrian Ramirez <adrram0202@gmail.com>
2023-11-08 10:10:07 -0600

    Add Git submodule for website test

---
+++ server.mjs
@@ -0,0 +1,17 @@
+import { default as express } from 'express';
+
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import * as url from 'node:url';
+
+const port = 8000;
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const root = path.join(__dirname, 'public');
+const template = path.join(__dirname, 'templates');
+
+let app = express();
+app.use(express.static(root));
+
+app.listen(port, () => {
+    console.log('Now listening on port ' + port);
+});
